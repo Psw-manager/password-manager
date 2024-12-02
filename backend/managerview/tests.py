70893c91473199302d3139817bbea1e9fc050cad
Jula434 <JuliaBanaszak2003@gmail.com>
2024-12-02 15:49:27 +0100

    hashowanie

--- backend/managerview/tests.py
+++ backend/managerview/tests.py
@@ -11,12 +11,12 @@ class ValidateTOTPTestCase(TestCase):
         self.user = User.objects.create(
             username="testuser",
             email="testuser@example.com",
-            password=make_password("securepassword123")  # Poprawne hashowanie hasła
+            password=make_password("securepassword123") 
         )
-        self.totp = pyotp.TOTP(self.user.totp_secret)  # TOTP dla użytkownika
+        self.totp = pyotp.TOTP(self.user.totp_secret)  #TOTP dla użytkownika
 
     def test_valid_totp_code(self):
-        valid_code = self.totp.now()  # Generuje aktualny kod TOTP
+        valid_code = self.totp.now()  #generuje aktualny kod TOTP
         response = self.client.post('/api/validate-totp/', {
             'email': self.user.email,
             'code': valid_code
@@ -27,7 +27,7 @@ class ValidateTOTPTestCase(TestCase):
     def test_invalid_totp_code(self):
         response = self.client.post('/api/validate-totp/', {
             'email': self.user.email,
-            'code': '000000'  # Fałszywy kod
+            'code': '000000'
         })
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data['error'], "Invalid TOTP code")
@@ -40,14 +40,14 @@ class UserAuthenticationTests(APITestCase):
             'email': 'testuser@example.com',
             'password': 'securepassword123'
         })
-        self.assertEqual(response.status_code, 201)  # Rejestracja zakończona sukcesem
+        self.assertEqual(response.status_code, 201)
 
-        # Sprawdzanie, czy użytkownik został poprawnie utworzony
+        #sprawdzanie, czy użytkownik został poprawnie utworzony
         user = User.objects.get(email='testuser@example.com')
-        self.assertTrue(check_password('securepassword123', user.password))  # Hasło jest prawidłowo zahashowane
+        self.assertTrue(check_password('securepassword123', user.password))  #haslo jest prawidłowo zahashowane
 
     def test_login_user(self):
-        # Tworzenie użytkownika w bazie danych
+        #tworzenie użytkownika w bazie danych
         user = User.objects.create(
             username='testuser',
             email='testuser@example.com',
@@ -57,5 +57,5 @@ class UserAuthenticationTests(APITestCase):
             'email': 'testuser@example.com',
             'password': 'securepassword123'
         })
-        self.assertEqual(response.status_code, 200)  # Logowanie zakończone sukcesem
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['message'], 'Login successful!')
